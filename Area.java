d5aef304384c51afccc6cacdfe18d5ecd5ec4087
Steve Thomas <st3233@drexel.edu>
2019-09-15 01:39:38 -0400

    Update Area.java

--- Area.java
+++ Area.java
@@ -13,7 +13,6 @@ public class Area
     
     String [] standardShapes = {"triangle", "square", "rectangle", "ellipse", "rhombus", "kite"};
     String [] polygons = {"pentagon", "hexagon", "heptagon", "octagon", "nonagon", "decagon"};
-    String [] otherShapes = {"circle", "trapezoid"};
     
     System.out.println("Welcome! What shape's area do you need to calculate today?");
     
@@ -34,38 +33,6 @@ public class Area
     }
     
 
-    else if (Arrays.asList(otherShapes).contains(shape.toLowerCase())) {
-    	if (shape == "trapezoid") {
-    		System.out.println("Enter the " + shape + "'s dimensions.");
-    		int base1 = scan.nextInt();
-    		int base2 = scan.nextInt();
-    		int height = scan.nextInt();
-    		
-    		while (base1<=0 || base2<=0 || height<=0) {
-            	System.out.println("Values cannot be less than or equal to zero. Please try again");
-            	base1 = scan.nextInt();
-            	base2 = scan.nextInt();
-            	height = scan.nextInt();
-            }
-            Comp compObject = new Comp();
-            compObject.CalculateTrapezoid(shape, base1, base2, height);
-        }
-    	
-    	else {
-    		System.out.println("Enter the circles radius.");
-    		int radius = scan.nextInt();
-    		
-    		while (radius<=0) {
-    			System.out.println("Radius canot be less than or equal to zero. Please try again.");
-    			radius = scan.nextInt();
-    		}
-    		
-    		Comp compObject = new Comp();
-            compObject.CalculateCircle(shape, radius);
-    	}
-    		
-    	
-    }
     else if (Arrays.asList(polygons).contains(shape.toLowerCase())){
     	System.out.println("Enter the length of the " + shape + "'s side.");
     	int side = scan.nextInt();
